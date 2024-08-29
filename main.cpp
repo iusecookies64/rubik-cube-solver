e0218793ac18fa8e8791a3bd9eab7f1ad10e152c
Tushar <traghav64@gmail.com>
2024-08-29 14:55:10 +0530

    .

--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@ signed main()
 {
     RubiksCube3dArray cube;
     cube.print();
+    cube.move(RubiksCube::MOVE::L);
+    cube.print();
+    cube.invert(RubiksCube::MOVE::L);
+    cube.print();
+    cube.randomShuffle(5);
+    cube.print();
     return 0;
 }
